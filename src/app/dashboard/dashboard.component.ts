dc405be4d3576a805e7bae7a440ae6179b61abfa
AngularV1 <careers.angular@gmail.com>
2024-02-05 18:43:23 +0530

    Add total amount calculation and display

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,32 +11,37 @@ export class DashboardComponent implements OnInit {
   productsArray: any = [
     {
       "id": 1,
-      "name": "Product 1",
-      "price": 19.99,
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo vitae urna suscipit feugiat."
+      "name": "Smartphone",
+      "price": 199.99,
+      "category": "Electronics",
+      "description": "Samsung Galaxy"
     },
     {
       "id": 2,
-      "name": "Product 2",
+      "name": "Running shoes",
       "price": 29.99,
+      "category": "Clothing and Fashion",
       "description": "Sed quis ante sed metus pretium gravida. Integer hendrerit justo eu libero convallis, nec vulputate ligula interdum."
     },
     {
       "id": 3,
-      "name": "Product 3",
+      "name": "Coffee Table",
       "price": 39.99,
+      "category": "Home and Furniture",
       "description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
     },
     {
       "id": 4,
-      "name": "Product 4",
+      "name": "Book Set",
       "price": 49.99,
+      "category": "Books and Media",
       "description": "Ut fermentum felis a mauris suscipit auctor. Quisque ut turpis vel libero ultricies bibendum."
     },
     {
       "id": 5,
-      "name": "Product 5",
+      "name": "Yoga Mat",
       "price": 59.99,
+      "category": "Sports and Outdoors",
       "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse aliquet arcu vitae feugiat bibendum."
     }
   ]
@@ -65,7 +70,7 @@ export class DashboardComponent implements OnInit {
       product.qty = 1;
       this.cart.splice(index, 1);
     }
-    this.calculateTotalAmount();  // Call the method to update total amount
+    this.calculateTotalAmount();
   }
 
   calculateTotalAmount() {
